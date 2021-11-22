d87d0c27fff83b2a892110408832946bbf0e7a2c
vasu <ee20947@bristol.ac.uk>
2021-11-22 16:39:59 +0000

    parallel

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,10 +1,7 @@
 package gol
 
 import (
-	"flag"
-	"net/rpc"
 	"strconv"
-	"uk.ac.bris.cs/gameoflife/stubs"
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
@@ -15,36 +12,6 @@ type distributorChannels struct {
 	ioFilename chan<- string
 	ioOutput   chan<- uint8
 	ioInput    <-chan uint8
-}
-
-func calculateAliveCells(p Params, world [][]byte) []util.Cell {
-
-	var alivecells []util.Cell
-
-	for row := 0; row < p.ImageWidth; row++ {
-		for col := 0; col < p.ImageHeight; col++ {
-
-			pair := util.Cell{}
-			currentCell := world[row][col]
-
-			if currentCell == 255 {
-				pair.X = col
-				pair.Y = row
-				alivecells = append(alivecells, pair)
-
-			}
-		}
-	}
-
-	return alivecells
-}
-
-func makeCall(client rpc.Client, world [][]byte, params Params) *stubs.Response {
-	params = Params(stubs.Params{Turns: params.Turns, Threads: params.Threads, ImageWidth: params.ImageWidth, ImageHeight: params.ImageHeight})
-	request := stubs.Request{World: world, P: stubs.Params(params)}
-	response := new(stubs.Response)
-	client.Call(stubs.Processsor, request, response)
-	return response
 }
 
 // distributor divides the work between workers and interacts with other goroutines.
@@ -79,25 +46,17 @@ func distributor(p Params, c distributorChannels) {
 	// the following would iterate calculating next state till done with turns
 	turn := 0
 
-	//server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
-	server := "127.0.0.1:8030"
-	flag.Parse()
-	client, b := rpc.Dial("tcp", server)
-	if b != nil {
-		print("there is no connrction")
+	for turn < p.Turns {
+		world = calculateNextState(p, world)
+		//print_world(req.World, req.P.ImageHeight, req.P.ImageWidth)
+		//fmt.Println()
+		turn++
 	}
-	defer client.Close()
 
-	resval := makeCall(*client, world, p)
-	world = resval.World
-	turn = resval.Turns
-
-	// report how many turns are over and how many alivers are remaining after each turn
-	// and send that to a channel which goes into the FinalTurnComplete
+	// send the next turn stuff thru to the response struct
+	p.Turns = turn
 
 	alivers := calculateAliveCells(p, world)
-
-	// TODO: Report the final state using FinalTurnCompleteEvent.
 
 	final := FinalTurnComplete{CompletedTurns: p.Turns, Alive: alivers}
 	c.events <- final
@@ -110,4 +69,86 @@ func distributor(p Params, c distributorChannels) {
 
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+
+}
+
+// report how many turns are over and how many alivers are remaining after each turn
+// and send that to a channel which goes into the FinalTurnComplete
+
+// TODO: Report the final state using FinalTurnCompleteEvent.
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+
+	//making a separate world to check without disturbing the actual world
+	testerworld := make([][]byte, len(world))
+	for i := range world {
+		testerworld[i] = make([]byte, len(world[i]))
+		copy(testerworld[i], world[i])
+	}
+
+	for row := 0; row < p.ImageHeight; row++ {
+		for col := 0; col < p.ImageWidth; col++ {
+
+			alivemeter := 0
+			for i := row - 1; i <= row+1; i++ {
+				for j := col - 1; j <= col+1; j++ {
+
+					if i == row && j == col {
+						continue
+					}
+
+					if world[((i + p.ImageWidth) % p.ImageWidth)][(j+p.ImageHeight)%p.ImageHeight] == 255 {
+						alivemeter++
+
+					}
+				}
+			}
+
+			// game of life conditions
+			if alivemeter < 2 || alivemeter > 3 {
+				testerworld[row][col] = 0
+			}
+			if alivemeter == 3 {
+				testerworld[row][col] = 255
+			}
+		}
+	}
+
+	return testerworld
+}
+
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+
+	var alivecells []util.Cell
+
+	for row := 0; row < p.ImageWidth; row++ {
+		for col := 0; col < p.ImageHeight; col++ {
+
+			pair := util.Cell{}
+			currentCell := world[row][col]
+
+			if currentCell == 255 {
+				pair.X = col
+				pair.Y = row
+				alivecells = append(alivecells, pair)
+
+			}
+		}
+	}
+
+	return alivecells
+}
+
+func makeMatrix(height, width int) [][]uint8 {
+	matrix := make([][]uint8, height)
+	for i := range matrix {
+		matrix[i] = make([]uint8, width)
+	}
+	return matrix
+}
+
+func makeImmutableMatrix(matrix [][]uint8) func(y, x int) uint8 {
+	return func(y, x int) uint8 {
+		return matrix[y][x]
+	}
 }
